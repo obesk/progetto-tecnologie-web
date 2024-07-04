f56f5fe0219d495bfa1427809247d7c3982914fb
Roberto Bertelli <robbybertelli@gmail.com>
2024-07-04 20:46:06 +0200

    Added notification to task (not working)
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- app/management/commands/update_auction_status.py
+++ app/management/commands/update_auction_status.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from app.models import Artwork
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 
 class Command(BaseCommand):
@@ -11,7 +13,32 @@ class Command(BaseCommand):
         artworks = Artwork.objects.filter(
             status=Artwork.Status.AUCTIONING, auction_end__lt=now
         )
+        channel_layer = get_channel_layer()
+        if channel_layer is None:
+            self.stdout.write(
+                self.style.ERROR("Channel layer is not configured properly")
+            )
+
         for artwork in artworks:
+            highest_bidder = artwork.Bids.order_by("-amount").first().customer
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Highest bidder for artwork "{artwork.name}" is {highest_bidder}'
+                )
+            )
+
+            # FIXME: the notification is not working
+            if channel_layer:
+                async_to_sync(channel_layer.group_send)(
+                    f"artwork_{artwork.id}",
+                    {
+                        "type": "send_message",
+                        "message": {
+                            "title": "Sold Notification",
+                            "body": f"The item {artwork.name} has been sold to {highest_bidder} for {artwork.price()}",
+                        },
+                    },
+                )
             artwork.status = Artwork.Status.SOLD
             artwork.save()
             self.stdout.write(
